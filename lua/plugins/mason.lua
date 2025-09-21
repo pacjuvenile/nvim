cd0d608fb2c2464759cddfae297b432e4d122d2d
packjuvenile <3283890195@qq.com>
2025-09-21 08:39:27 +0800

    modified:   lazy-lock.json 	new file:   lua/plugins/mason.lua

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  "williamboman/mason.nvim",
+  event = "VeryLazy",
+  opts = {},
+  config = function (_, opts) 
+    require("mason").setup(opts)
+    local registry = require("mason-registry")
+    local success, package = pcall(registry.get_package, "lua-language-server")
+    if success and not package:is_installed() then
+      package:install()
+    end
+  end
+}
