3509cd06d7925982d5628fa947ffe487d75fbed0
packjuvenile <3283890195@qq.com>
2025-09-21 17:34:46 +0800

    modified:   lua/core/keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,6 +4,11 @@ vim.keymap.set("i", "<A-l>", [[<Right>]], { noremap = true })                   
 vim.keymap.set("i", "<A-h>", [[<Left>]], { noremap = true })                                -- 替换方向键左移
 vim.keymap.set("i", "<A-k>", [[<Up>]], { noremap = true })                                  -- 替换方向键上移
 vim.keymap.set("i", "<A-j>", [[<Down>]], { noremap = true })                                -- 替换方向键下移
+vim.keymap.set("i", "jj", function ()
+  if vim.fn.getcmdwintype() ~= "" then
+    return "<Esc>"                  -- 命令窗口返回普通模式
+  end
+end, { expr = true, noremap = true })
 
 vim.keymap.set("n", "<A-S-h>",[[<C-w>h]], { noremap = true }) -- 移至左窗口
 vim.keymap.set("n", "<A-S-l>",[[<C-w>l]], { noremap = true }) -- 移至右窗口
@@ -16,20 +21,14 @@ vim.keymap.set({ "n", "v" }, "p", [["+p]], { noremap = true })      -- 粘贴
 vim.keymap.set("i", "<A-p>", [[<C-r>+<Esc>]], { noremap = true })   -- 插入模式粘贴
 vim.keymap.set("n", ":", [[q:i]], { noremap = true })               -- 进入命令窗口
 
-vim.keymap.set("n" , "<Esc>", function ()
+vim.keymap.set({ "n", "i" } , "<Esc>", function ()
   if vim.fn.getcmdwintype() ~= "" then
       return "<C-c><C-c>"           -- 退出命令窗口
+  elseif vim.fn.mode() == "n" then
+      return "<Cmd>nohl<CR>"        -- 取消高亮
   else
-    return "<Cmd>nohl<CR>"          -- 取消高亮
+      return "<Esc>"                -- 返回普通模式
   end
-end, { expr = true, noremap = true, silent = true })
+end, { expr = true, noremap = true })
 
-vim.keymap.set("n", "<leader>m", [[:%s.\r..g<CR>]], { noremap = true, silent = true })  -- 删除Windows风格的换行符
-
-
-
-
-
-
-
-
+vim.keymap.set("n", "<A-m>", [[:%s.\r..g<CR>]], { noremap = true, silent = true })  -- 删除Windows风格的换行符
