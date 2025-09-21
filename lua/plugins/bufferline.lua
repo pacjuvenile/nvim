bd6dc502932859d61b615e77352b5374143762f9
packjuvenile <3283890195@qq.com>
2025-09-21 11:58:41 +0800

    modified:   lazy-lock.json 	modified:   lua/plugins/bufferline.lua 	modified:   lua/plugins/mason.lua

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,7 +4,7 @@ return {
   keys = {
     { "<leader>bh", "<CMD>BufferLineCyclePrev<CR>", silent = true },
     { "<leader>bl", "<CMD>BufferLineCycleNext<CR>", silent = true },
-    { "<leader>bd", "<CMD>bdelete<CR>", silent = true },
+    { "<leader>bd", "<CMD>bdelete!<CR>", silent = true },
     { "<leader>bo", "<CMD>BufferLineCloseOthers<CR>", silent = true },
     { "<leader>bp", "<CMD>BufferLinePick<CR>", silent = true }
   },
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,13 +1,49 @@
 return {
   "mason-org/mason.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mason-org/mason-lspconfig.nvim"
+  },
+  lazy = true,
   event = "VeryLazy",
   opts = {},
-  config = function (_, opts) 
+  config = function (_, opts)
     require("mason").setup(opts)
     local registry = require("mason-registry")
-    local success, package = pcall(registry.get_package, "lua-language-server")
-    if success and not package:is_installed() then
-      package:install()
+    local function setup(mason_lsp,config)
+      local success, package = pcall(registry.get_package, mason_lsp)
+      if success and not package:is_installed() then
+        package:install()
+      end
+      local nvim_lsp = require("mason-lspconfig.mappings").get_mason_map().package_to_lspconfig[mason_lsp]
+      vim.lsp.config(nvim_lsp, config)
     end
+
+    local lsp_config_table = {
+      -- lua lsp配置
+      ["lua-language-server"] = {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }
+            }
+          }
+        }
+      },
+      ["python-lsp-server"] = {},
+      ["html-lsp"] = {},
+      ["css-lsp"] = {},
+      ["typescript-language-server"] = {}
+    }
+
+    for mason_lsp, config in pairs(lsp_config_table) do
+      setup(mason_lsp, config)
+    end
+
+    vim.cmd("LspStart")
+    vim.diagnostic.config({
+      virtual_text = true,
+      update_in_insert = true
+    })
   end
 }
