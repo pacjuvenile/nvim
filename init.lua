f6977f7987455884a1c85f8830a064fe96e61f68
packjuvenile <3283890195@qq.com>
2025-10-19 09:35:57 +0800

    modified:   init.lua

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
--- 插件管理
-require("plugin_manager.lazy")
-
 -- 选项设置
 require("settings.options")
 -- 键位配置
 require("settings.keymaps")
 -- 自动命令
 require("settings.autocmds")
+
+-- 插件管理
+require("plugin_manager.lazy")
