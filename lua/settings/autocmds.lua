3b61c1de819ec847726b14fc5d5e6b7d8e12575a
packjuvenile <3283890195@qq.com>
2025-10-27 23:54:47 +0800

    modified:   lua/settings/autocmds.lua

--- lua/settings/autocmds.lua
+++ lua/settings/autocmds.lua
@@ -3,12 +3,32 @@ local im_autogroup = vim.api.nvim_create_augroup("ImAutoGroup", { clear = true }
 vim.api.nvim_create_autocmd("InsertLeave", {
     group = im_autogroup,
     callback = function()
-        vim.cmd(":silent :!/mnt/c/Users/sunny/app/im-select/im-select.exe 1033")
+        vim.cmd([[silent !/mnt/c/Users/sunny/app/im-select/im-select.exe 1033]])
     end
 })
+
 vim.api.nvim_create_autocmd("InsertEnter", {
   group = im_autogroup,
-  callback = function ()
-    vim.cmd(":silent :!/mnt/c/Users/sunny/app/im-select/im-select.exe 2052")
+  callback = function()
+    vim.cmd([[silent !/mnt/c/Users/sunny/app/im-select/im-select.exe 2052]])
   end
 })
+
+-- 打开文件保留上次光标位置
+local view_autogroup = vim.api.nvim_create_augroup("ViewAutoGroup", { clear = true })
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = view_autogroup,
+    callback = function()
+        if vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
+            vim.cmd([[normal! `"]])
+        end
+    end
+})
+-- vim.api.nvim_create_autocmd("BufWinLeave", {
+--     group = view_autogroup,
+--     callback = function()
+--         if vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
+--             vim.cmd([[mkview]])
+--         end
+--     end
+-- })
