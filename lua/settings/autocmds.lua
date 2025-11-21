fe5fc0341b9fc5e4aed9d74bfedec5fab91a5258
packjuvenile <3283890195@qq.com>
2025-11-21 14:35:23 +0800

    modified:   .gitignore 	modified:   lua/settings/autocmds.lua

--- lua/settings/autocmds.lua
+++ lua/settings/autocmds.lua
@@ -1,19 +1,3 @@
--- 输入法调整
--- local im_augroup = vim.api.nvim_create_augroup("ImAugroup", { clear = true })
--- vim.api.nvim_create_autocmd("InsertLeave", {
---     group = im_augroup,
---     callback = function()
---         vim.cmd([[silent !/mnt/c/Users/sunny/app/im-select/im-select.exe 1033]])
---     end
--- })
---
--- vim.api.nvim_create_autocmd("InsertEnter", {
---   group = im_augroup,
---   callback = function()
---     vim.cmd([[silent !/mnt/c/Users/sunny/app/im-select/im-select.exe 2052]])
---   end
--- })
-
 -- 打开文件保留上次光标位置
 local view_augroup = vim.api.nvim_create_augroup("ViewAugroup", { clear = true })
 vim.api.nvim_create_autocmd("BufReadPost", {
