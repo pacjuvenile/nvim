a75e61a623b0e84c359ba78c7cd9a24d3079f121
packjuvenile <3283890195@qq.com>
2026-01-23 00:36:20 +0800

    modified:   init.lua 	modified:   lua/settings/options.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 -- 全局设置
-vim.g.loaded_netrw = 1          -- disable netrw
-vim.g.loaded_netrwPlugin = 1    -- disable netrw plugin
-vim.opt.encoding = "utf-8"      -- enable utf-8
-vim.opt.termguicolors = true    -- enable 24-bit color
+vim.g.loaded_netrw = 1          -- 禁用netrw
+vim.g.loaded_netrwPlugin = 1    -- 禁用netrw插件
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -1,25 +1,44 @@
-vim.opt.number         = true   -- 显示当前行号
-vim.opt.relativenumber = true   -- 显示相对行号
+-- 文件编码
+vim.opt.encoding       = "utf-8" -- 启用utf-8
 
-vim.opt.shiftwidth     = 2      -- 缩进宽度2个空格
-vim.opt.tabstop        = 8      -- 制表符宽度8个空格
-vim.opt.softtabstop    = 2      -- Tab/Backspace对应2个空格宽度
-vim.opt.expandtab      = true   -- Tab转换为空格
+-- 高亮颜色
+vim.opt.termguicolors  = true -- 启用24-bit颜色
 
-vim.opt.cursorline     = true   -- 高亮光标所在行
+-- 行号
+vim.opt.number         = true -- 显示当前行号
+vim.opt.relativenumber = true -- 显示相对行号
 
-vim.opt.splitright     = true   -- 新窗口分割到右侧
-vim.opt.splitbelow     = true   -- 新窗口分割到下方
+-- Tab行为
+vim.opt.shiftwidth     = 2    -- 缩进宽度2个空格
+vim.opt.tabstop        = 8    -- 制表符宽度8个空格
+vim.opt.softtabstop    = 2    -- Tab/Backspace对应2个空格宽度
+vim.opt.expandtab      = true -- Tab转换为空格
 
-vim.opt.cmdheight      = 1      -- 命令行与状态栏共享空间
-vim.opt.laststatus     = 3      -- 所有window共用底部状态栏
-vim.opt.cmdwinheight   = 1      -- 设置命令窗口大小为1
+-- 新行缩进
+vim.opt.smartindent    = true -- 启动语义缩进
 
-vim.opt.ignorecase     = true   -- 不输入大写则大小写不敏感
-vim.opt.smartcase      = true   -- 输入大写则大写敏感
+-- 光标
+vim.opt.cursorline     = true -- 高亮光标所在行
 
+-- 命令行
+vim.opt.cmdheight      = 1 --
+vim.opt.laststatus     = 3 -- 所有window共用底部状态栏
+vim.opt.cmdwinheight   = 1 -- 设置命令窗口大小为1
+
+-- 搜索设置
+vim.opt.ignorecase     = true -- 不输入大写则大小写不敏感
+vim.opt.smartcase      = true -- 输入大写则大写敏感
+
+-- 折叠设置
 vim.opt.foldmethod     = "expr" -- 折叠方法基于表达式
 vim.opt.foldlevel      = 99     -- 大于折叠层级的行会被折叠
+
+-- 窗口分割
+vim.opt.splitright     = true -- 新窗口分割到右侧
+vim.opt.splitbelow     = true -- 新窗口分割到下方
+
+-- 文件缓存
+vim.opt.swapfile       = false -- 关闭交换文件
 
 vim.diagnostic.config({
   virtual_text = true,
