e17fd4e52fffa185a0892c47e3306298c9d4f35e
packjuvenile <3283890195@qq.com>
2026-01-27 00:12:49 +0800

    modified:   lua/languages/python.lua

--- lua/languages/python.lua
+++ lua/languages/python.lua
@@ -4,5 +4,22 @@ M.ts_config = {
   parser = { "python" }
 }
 
+M.ls_config = {
+  name = "pyright",
+  cmd = { "pyright-langserver" },
+  filetypes = { "python" },
+  root_markers = {
+    ".git"
+  },
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "openFilesOnly"
+      }
+    }
+  }
+}
 
 return M
