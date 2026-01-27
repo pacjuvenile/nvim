de8716a3b0db152f3be5f465f7f932fc306e69a9
packjuvenile <3283890195@qq.com>
2026-01-27 09:26:45 +0800

    modified:   lua/languages/rust.lua

--- lua/languages/rust.lua
+++ lua/languages/rust.lua
@@ -5,14 +5,41 @@ M.ts_config = {
 }
 
 M.ls_config = {
-  name = nil,
-  cmd = { "" } ,
+  name = "rust_analyzer",
+  cmd = { "rust-analyzer" } ,
   filetypes = { "rust" },
   root_markers = {
     ".git"
   },
+  capabilities = {
+      experimental = {
+        serverStatusNotification = true,
+        commands = {
+          commands = {
+            'rust-analyzer.showReferences',
+            'rust-analyzer.runSingle',
+            'rust-analyzer.debugSingle',
+          },
+        },
+      },
+  },
   settings = {
-  }
+    ['rust-analyzer'] = {
+      lens = {
+        debug = { enable = true },
+        enable = true,
+        implementations = { enable = true },
+        references = {
+          adt = { enable = true },
+          enumVariant = { enable = true },
+          method = { enable = true },
+          trait = { enable = true },
+        },
+        run = { enable = true },
+        updateTest = { enable = true },
+      },
+    },
+  },
 }
 
 return M
