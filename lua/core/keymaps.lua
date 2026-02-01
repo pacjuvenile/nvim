ada2bccefc937e213f5da490804ae12e025df79b
packjuvenile <3283890195@qq.com>
2026-02-01 19:29:22 +0800

    modified:   lua/core/keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,16 +1,12 @@
 vim.keymap.set("n", "j", [[v:count == 0 ? "gj" : "j"]], { expr = true }) -- 视觉行下移
 vim.keymap.set("n", "k", [[v:count == 0 ? "gk" : "k"]], { expr = true }) -- 视觉行上移
-vim.keymap.set("i", "<A-h>", [[<Left>]])                                 -- 替换方向键左移
-vim.keymap.set("i", "<A-j>", [[<Down>]])                                 -- 替换方向键下移
-vim.keymap.set("i", "<A-k>", [[<Up>]])                                   -- 替换方向键上移
-vim.keymap.set("i", "<A-l>", [[<Right>]])                                -- 替换方向键右移
 vim.keymap.set("n", "H", [[<Cmd>bprevious<cr>]], { silent = true })      -- 切换至上一个buffer
 vim.keymap.set("n", "L", [[<Cmd>bnext<cr>]], { silent = true })          -- 切换至下一个buffer
 
 vim.keymap.set({ "n", "v" }, "y", [["+y]])                               -- 复制
 vim.keymap.set("v", "x", [["+x]])                                        -- 剪切
 vim.keymap.set({ "n", "v" }, "p", [["+p]])                               -- 粘贴
-vim.keymap.set("i", "<A-p>", [[<C-r>+]])                                 -- 插入模式下粘贴
+vim.keymap.set("i", "<C-v>", [[<C-r>+]])                                 -- 插入模式下粘贴
 
 vim.keymap.set("n", ":", [[q:]])                                         -- 进入命令窗口
 vim.keymap.set("n", "<Esc>", function()
