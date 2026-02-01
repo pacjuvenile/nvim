c65b49bbaff355a581cdd860ec77e1e3b64f5ec6
packjuvenile <3283890195@qq.com>
2026-02-01 11:09:06 +0800

    modified:   lua/plugins/writing/markdown-preview.lua 	deleted:    lua/plugins/writing/peek.lua

--- lua/plugins/writing/markdown-preview.lua
+++ lua/plugins/writing/markdown-preview.lua
@@ -7,8 +7,19 @@ M.lazy = true
 M.ft = "markdown"
 
 M.init = function()
+  vim.g.mkdp_auto_start = 0
+  vim.g.mkdp_auto_close = 0
+  vim.g.mkdp_browserfunc = "OpenQuteWin"
+  vim.cmd [[
+    function! OpenQuteWin(url)
+      " 核心逻辑：wsl.exe调用Windows端qutebrowser，传入预览URL
+      " 后台执行(&)：避免阻塞Neovim，浏览器启动后不卡终端
+      execute '!wsl.exe /mnt/c/Users/sunny/app/qutebrowser/qutebrowser.exe ' . a:url . ' &'
+      " 静默执行：消除Neovim的"按回车继续"提示
+      redraw!
+    endfunction
+  ]]
   vim.g.mkdp_filetypes = { "markdown" }
-  vim.g.mkdp_browser = "/mnt/c/Users/sunny/app/qutebrowser/qutebrowser"
 end
 
 return M
