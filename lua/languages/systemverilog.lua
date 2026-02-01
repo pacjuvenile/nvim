a7361e054da2c7950964671b31f46ed16d550de4
packjuvenile <3283890195@qq.com>
2026-02-01 21:40:37 +0800

    new file:   lua/languages/systemverilog.lua 	deleted:    lua/languages/verilog.lua

---
+++ lua/languages/systemverilog.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.ts_config = {
+  parser = { "systemverilog" }
+}
+
+M.ls_config = {
+  name = "svls",
+  cmd = { "svls" },
+  filetypes = { "verilog", "systemverilog" },
+  root_markers = { ".git" }
+}
+
+return M
--- lua/languages/verilog.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.ts_config = {
-  parser = { "systemverilog" }
-}
-
-return M
