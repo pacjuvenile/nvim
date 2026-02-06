f4387115d698c2089100f4a277903d25844ef74d
packjuvenile <3283890195@qq.com>
2026-02-06 15:48:56 +0800

    modified:   init.lua 	modified:   lua/core/options.lua

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
 -- 字符编码
-vim.cmd("language en_US.UTF-8")
-vim.g.encoding = "UTF-8"
+vim.cmd('language en_US.UTF-8')
+vim.g.encoding = 'UTF-8'
 
 -- 全局映射
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
 
 -- 关闭内置缩进
 vim.cmd('filetype indent off')
 
 -- 选项设置
-require("core.options")
+require('core.options')
 -- 插件管理
-require("plugin-manager")
+require('plugin-manager')
 -- 语言设置
-require("core.lsp")
+require('core.lsp')
 -- 键位配置
-require("core.keymaps")
+require('core.keymaps')
 -- 自动命令
-require("core.autocmds")
+require('core.autocmds')
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,5 +1,5 @@
 -- 文件编码
-vim.opt.fileencoding   = "utf-8" -- 启用utf-8
+vim.opt.fileencoding   = 'utf-8' -- 启用utf-8
 
 -- 同步外部修改
 vim.opt.autoread       = true -- 自动读取外部修改
@@ -25,7 +25,7 @@ vim.opt.expandtab      = true -- Tab转换为空格
 vim.opt.cursorline     = true -- 高亮光标所在行
 
 -- 符号列
-vim.opt.signcolumn     = "no" -- 不显示左侧符号列
+vim.opt.signcolumn     = 'no' -- 不显示左侧符号列
 
 -- 命令行
 vim.opt.cmdheight      = 1 -- 始终显示一行的底部命令行区域
@@ -39,7 +39,7 @@ vim.opt.ignorecase     = true -- 不输入大写则大小写不敏感
 vim.opt.smartcase      = true -- 输入大写则大写敏感
 
 -- 数字格式设置（<C-a>加1,<C-x>减1）
-vim.opt.nrformats      = "bin,hex,alpha" -- 二进制，十六进制和字母都视为数字
+vim.opt.nrformats      = 'bin,hex,alpha' -- 二进制，十六进制和字母都视为数字
 
 -- 折叠设置
 vim.opt.foldlevel      = 99 -- 大于折叠层级的行会被折叠
