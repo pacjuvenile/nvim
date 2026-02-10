7ab38f3aa033c6842f1ec9822603941a71dd0b81
packjuvenile <3283890195@qq.com>
2026-02-10 18:30:01 +0800

    modified:   lua/languages/systemverilog.lua

--- lua/languages/systemverilog.lua
+++ lua/languages/systemverilog.lua
@@ -4,18 +4,18 @@ M.ts_config = {
   parser = { 'systemverilog' }
 }
 
--- M.ls_config = {
---   name = 'svls',
---   cmd = { 'svls' },
---   filetypes = { 'systemverilog' },
---   root_markers = { '.git' },
---   settings = {
---     svls = {
---       option = {
---           linter = false
---       }
---     }
---   }
--- }
+M.ls_config = {
+  name = 'svls',
+  cmd = { 'svls' },
+  filetypes = { 'systemverilog', 'verilog' },
+  root_markers = { '.git' },
+  settings = {
+    svls = {
+      option = {
+          linter = false
+      }
+    }
+  }
+}
 
 return M
