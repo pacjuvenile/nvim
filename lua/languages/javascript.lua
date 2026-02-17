ba0de30ae9ce8d19fd1861d3044a8411b23bf025
packjuvenile <3283890195@qq.com>
2026-02-17 17:23:29 +0800

    modified:   lua/languages/javascript.lua

--- lua/languages/javascript.lua
+++ lua/languages/javascript.lua
@@ -10,14 +10,76 @@ M.ls_config = {
 	filetypes = {
 		'javascript',
 		'javascriptreact',
-		'javascript.jsx',
 		'typescript',
 		'typescriptreact',
-		'typescript.tsx'
 	},
-	root_dir = {
-		'.git'
-	}
+	root_dir = function(bufnr, on_dir)
+    -- The project root is where the LSP can be started from
+    -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+    -- We select then from the project root, which is identified by the presence of a package
+    -- manager lock file.
+    local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+    -- Give the root markers equal priority by wrapping them in a table
+    root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
+      or vim.list_extend(root_markers, { '.git' })
+    -- exclude deno
+    local deno_root = vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc' })
+    local deno_lock_root = vim.fs.root(bufnr, { 'deno.lock' })
+    local project_root = vim.fs.root(bufnr, root_markers)
+    if deno_lock_root and (not project_root or #deno_lock_root > #project_root) then
+      -- deno lock is closer than package manager lock, abort
+      return
+    end
+    if deno_root and (not project_root or #deno_root >= #project_root) then
+      -- deno config is closer than or equal to package manager lock, abort
+      return
+    end
+    -- project is standard TS, not deno
+    -- We fallback to the current working directory if no project root is found
+    on_dir(project_root or vim.fn.getcwd())
+  end,
+  handlers = {
+    -- handle rename request for certain code actions like extracting functions / types
+    ['_typescript.rename'] = function(_, result, ctx)
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      vim.lsp.util.show_document({
+        uri = result.textDocument.uri,
+        range = {
+          start = result.position,
+          ['end'] = result.position,
+        },
+      }, client.offset_encoding)
+      vim.lsp.buf.rename()
+      return vim.NIL
+    end,
+  },
+  commands = {
+    ['editor.action.showReferences'] = function(command, ctx)
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      local file_uri, position, references = unpack(command.arguments)
+
+      local quickfix_items = vim.lsp.util.locations_to_items(references --[[@as any]], client.offset_encoding)
+      vim.fn.setqflist({}, ' ', {
+        title = command.title,
+        items = quickfix_items,
+        context = {
+          command = command,
+          bufnr = ctx.bufnr,
+        },
+      })
+
+      vim.lsp.util.show_document({
+        uri = file_uri --[[@as string]],
+        range = {
+          start = position --[[@as lsp.Position]],
+          ['end'] = position --[[@as lsp.Position]],
+        },
+      }, client.offset_encoding)
+      ---@diagnostic enable: assign-type-mismatch
+
+      vim.cmd('botright copen')
+    end,
+  },
 }
 
 return M
