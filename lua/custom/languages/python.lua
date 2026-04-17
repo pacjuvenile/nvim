3e56590d4d67aa55d0b11b92247bd0bdb61422da
packjuvenile <3283890195@qq.com>
2026-04-17 10:47:40 +0800

    modified:   lua/custom/languages/python.lua

--- lua/custom/languages/python.lua
+++ lua/custom/languages/python.lua
@@ -5,24 +5,24 @@ M.ts_config = {
   parser = { 'python' }
 }
 
-M.ls_config = {
-  name = 'pyright',
-  cmd = { 'pyright-langserver', '--stdio' },
-  filetypes = { 'python' },
-  root_markers = {
-    '.git',
-    'pyproject.toml',
-  },
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-        diagnosticMode = 'openFilesOnly',
-      }
-    }
-  }
-}
+-- M.ls_config = {
+--   name = 'pyright',
+--   cmd = { 'pyright-langserver', '--stdio' },
+--   filetypes = { 'python' },
+--   root_markers = {
+--     '.git',
+--     'pyproject.toml',
+--   },
+--   settings = {
+--     python = {
+--       analysis = {
+--         autoSearchPaths = true,
+--         useLibraryCodeForTypes = true,
+--         diagnosticMode = 'openFilesOnly',
+--       }
+--     }
+--   }
+-- }
 
 -- local python_augroup = vim.api.nvim_create_augroup('Python', { clear = true })
 -- vim.api.nvim_create_autocmd('FileType', {
