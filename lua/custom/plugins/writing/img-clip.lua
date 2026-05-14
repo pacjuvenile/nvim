db45adacced916fdf0aa091b8ac9f73b4835abdc
packjuvenile <3283890195@qq.com>
2026-05-14 16:16:40 +0800

    202605141616

--- lua/custom/plugins/writing/img-clip.lua
+++ lua/custom/plugins/writing/img-clip.lua
@@ -8,19 +8,20 @@ M.ft = "markdown"
 
 M.config = function()
   require("img-clip").setup({
+		default = {
+			dir_path = "assets"
+		},
     filetypes = {
       markdown = {
         -- 模板选项
-        template = "<img src=\"$FILE_PATH\" width=\"100%\">",
+        template = '<img src="$FILE_PATH" width="100%">',
         insert_mode_after_paste = false,
 
         -- 提示选项
-        prompt_for_file_name = false,
+        prompt_for_file_name = true
       }
     }
   })
-
-  vim.keymap.set("n", "<leader>p", "<cmd>PasteImage<cr>", { silent = true, desc = "Paste image from system clipboard" })
 end
 
 return M
